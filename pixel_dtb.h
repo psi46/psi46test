332458703fad1385c604839e38d48608a9a60c37
meierb <beat.meier@psi.ch>
2015-01-20 10:36:35 +0100

    Functions for DTB trigger added

--- pixel_dtb.h
+++ pixel_dtb.h
@@ -297,6 +297,33 @@ public:
 	RPC_EXPORT void Pg_Loop(uint16_t period);
 
 
+	// --- Trigger ----------------------------------------------------------
+	#define TRG_SEL_ASYNC      0x100
+	#define TRG_SEL_SYNC       0x080
+	#define TRG_SEL_SINGLE     0x040
+	#define TRG_SEL_GEN        0x020
+	#define TRG_SEL_PG         0x010
+	#define TRG_SEL_SINGLE_DIR 0x008
+	#define TRG_SEL_PG_DIR     0x004
+	#define TRG_SEL_CHAIN      0x002
+	#define TRG_SEL_SYNC_OUT   0x001
+	RPC_EXPORT void Trigger_Select(uint16_t mask);
+
+	RPC_EXPORT void Trigger_Delay(uint8_t delay);
+
+	RPC_EXPORT void Trigger_Timeout(uint16_t timeout);
+
+	RPC_EXPORT void Trigger_SetGenPeriodic(uint32_t periode);
+	RPC_EXPORT void Trigger_SetGenRandom(uint32_t rate);
+
+	#define TRG_SEND_SYN   1
+	#define TRG_SEND_TRG   2
+	#define TRG_SEND_RSR   4
+	#define TRG_SEND_RST   8
+	#define TRG_SEND_CAL  16
+	RPC_EXPORT void Trigger_Send( uint8_t send);
+
+
 	// --- data aquisition --------------------------------------------------
 	RPC_EXPORT uint32_t Daq_Open(uint32_t buffersize = 10000000, uint8_t channel = 0);
 	RPC_EXPORT void Daq_Close(uint8_t channel = 0);
